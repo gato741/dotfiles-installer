749902e5c2a0a4f4b209a4ea0f460d4ce47fbbae
Gato <maksymiliancapala1@gmail.com>
2025-05-02 20:34:19 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+# MAKE SURE THAT ~/.local/share/fonts EXISTS!
+# MAKE SURE THAT GIT, WGET AND UNZIP ARE INSTALLED!
+# MAKE SURE THAT PICOM IS INSTALLED
+
+cd $HOME/.local/share/fonts
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Terminus.zip
+unzip Terminus.zip
+rm Terminus.zip
+
+mkdir $HOME/suckless
+cd $HOME/suckless
+
+git clone https://github.com/gato741/dwm
+git clone https://github.com/gato741/st
+git clone https://github.com/gato741/slstatus
+git clone git://git.suckless.org/dmenu
+
+cd dwm
+sudo make clean install
+cd ..
+
+cd st
+sudo make clean install
+cd ..
+
+cd slstatus
+sudo make clean install
+cd ..
+
+cd dmenu
+sudo make clean install
+cd $HOME
+
+git clone https://github.com/gato741/dotfiles
+mv $HOME/dotfiles/backgrounds ~
+mv $HOME/dotfiles/.vimrc ~
+mv $HOME/dotfiles/.xinitrc ~
